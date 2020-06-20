cbea474f341f815eecd2853c77c74b1184c4203a
Tim Boeckmann <trb@bespecific.co.uk>
2020-06-20 12:43:23 +0100

    Update install.sh
    Fixing https://github.com/mailchain/mailchain/issues/601

--- static/install.sh
+++ static/install.sh
@@ -6,7 +6,7 @@ set -eu
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
     grep '"tag_name":' |                                            # Get tag line
-    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+    sed -E 's/.*"v([^"]+)".*/\1/'                                   # Pluck JSON value, removing 'v' from version number
 }
 
 # Usage
